7940de6b75c9369ff595a8026b2998eb68cdeaf2
Lukas Feiler <lukasfeiler@php.net>
2009-03-01 12:59:52 +0000

    new setConfigPath() and getConfigPath() methods
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Query2XML/trunk@276637 c90b9560-bf6c-de11-be94-00142212c4b1

--- XML/Query2XML/Data.php
+++ XML/Query2XML/Data.php
@@ -11,6 +11,7 @@
  * @license   http://www.gnu.org/copyleft/lesser.html  LGPL Version 2.1
  * @version   CVS: $Id$
  * @link      http://pear.php.net/package/XML_Query2XML
+ * @access    private
  */
 
 /**
@@ -28,16 +29,63 @@ require_once 'XML/Query2XML/Callback.php';
  * @license   http://www.gnu.org/copyleft/lesser.html  LGPL Version 2.1
  * @version   Release: @package_version@
  * @link      http://pear.php.net/package/XML_Query2XML
- * @since     Release 1.8.0RC1
+ * @access    private
+ * @since     Release 1.7.1RC1
  */
 abstract class XML_Query2XML_Data implements XML_Query2XML_Callback
 {
+    /**
+     * The configuration path; it is used for exception messages.
+     * @var string
+     */
+    private $_configPath = '';
+    
+    /**
+     * This method will be called by XML_Query2XML to create a new instance
+     * of a class extending this class.
+     *
+     * @param mixed  $primaryArg The primary argument: if it's is a
+     *                           Data Processor or Condition Class this will
+     *                           be a preprocessor (i.e. an instance of
+     *                           XML_Query2XML_Data); if it's a Data Source
+     *                           this will most likely be a string.
+     * @param string $configPath The configuration path within the $options
+     *                           array.
+     *
+     * @return XML_Query2XML_Data_Processor
+     */
+    public abstract function create($primaryArg, $configPath);
+                                    
     /**
      * Returns the first pre-processor in the chain.
      *
      * @return XML_Query2XML_Data
      */
     public abstract function getFirstPreProcessor();
+    
+    /**
+     * Set the configuration path to be used for exception messages.
+     *
+     * @param string $configPath The configuration path
+     * @return void
+     */
+    protected function setConfigPath($configPath)
+    {
+        if ($configPath) {
+            $configPath .= ': ';
+        }
+        $this->_configPath = $configPath;
+    }
+    
+    /**
+     * Return the configuration path to be used for exception messages.
+     *
+     * @return string The configuration path.
+     */
+    protected function getConfigPath()
+    {
+        return $this->_configPath;
+    }
     
     /**
      * Returns a textual representation of this instance.
